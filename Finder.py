8830bea376595e72da1eb38d7b55539863ed8132
Danil <Dankwaso@gmail.com>
2019-10-08 21:27:17 +0300

    Add files via upload

--- Finder.py
+++ Finder.py
@@ -3,40 +3,43 @@ from bs4 import BeautifulSoup
 import json
 import codecs
 import datetime
-from flask import Flask
+from flask import Flask, render_template
 
-class Finder(object):
+#app = Flask(__name__)
+#@app.route('/')
 
-    def __init__(self,url):
-        self.url = url
-    def url_stuff(self):
-        resp = requests.get(self.url)
-        return resp
-    def parcer(self, resp):
-        topics = {}
-        now = datetime.datetime.now()
-        topics["url"] = [self.url]
-        topics["creationDate"] = [str(now)]
-        topics["articles"] = []
-        resp = requests.get(self.url)
-        if resp.status_code == 200:
-            soup3 = BeautifulSoup(resp.text, 'html.parser')
-            l3 = soup3.find("div", {"class": "lent-left"})
-            for i in l3.findAll("div", "title lent-title"):
-                topics["articles"].append({"Title": i.text})
-        else:
-            print("All for now")
-        return topics
-    def write_json(self,topics):
-        with codecs.open("StopGame.json", "w", encoding="utf-8") as outfile:
-            json.dump(topics, outfile, indent=4, ensure_ascii=False, separators=(',', ': '))
-        outfile.close()
+def get_html_page(url):
+    resp = requests.get(url)
+    return resp
+def find_articles(resp, url):
+    topics = {}
+    now = datetime.datetime.now()
+    topics["url"] = [url]
+    topics["creationDate"] = [str(now)]
+    topics["articles"] = []
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        soup3 = BeautifulSoup(resp.text, 'html.parser')
+        l3 = soup3.find("div", {"class": "lent-left"})
+        for i in l3.findAll("div", "title lent-title"):
+            topics["articles"].append({"Title": i.text})
+    else:
+        print("All for now")
+    return topics
+def publish_report(topics):
+    with codecs.open("StopGame.json", "w", encoding="utf-8") as outfile:
+        json.dump(topics, outfile, indent=4, ensure_ascii=False, separators=(',', ': '))
+    outfile.close()
 
 
-def main():
-    Parcer = Finder("https://stopgame.ru/news")
-    resp3 = Parcer.url_stuff()
-    top = Parcer.parcer(resp3)
-    Parcer.write_json(top)
-if __name__=="__main__":
-    main()
+'''def main():
+    url3="https://stopgame.ru/news"
+    resp3 = get_html_page(url3)
+    top = find_articles(resp3)
+    publish_report(top)
+    #render_template('news.html', articles=articles)'''
+
+
+'''if __name__=="__main__":
+    #app.run(host='localhost', port=8000)
+    main()'''
---
+++ main.py
@@ -0,0 +1,7 @@
+from Lab1.Finder import get_html_page, find_articles, publish_report
+
+url3="https://stopgame.ru/news"
+resp3 = get_html_page(url3)
+top = find_articles(resp3, url3)
+publish_report(top)
+#render_template('news.html', articles=articles)
